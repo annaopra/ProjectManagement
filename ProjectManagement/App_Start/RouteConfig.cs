19088b5426091b4f8deee0a1c38120bebf728ea2
unknown <ana.opra@XDR117.xoomworks.local>
2017-01-27 17:11:59 +0200

    Lab6 Ex1

--- ProjectManagement/App_Start/RouteConfig.cs
+++ ProjectManagement/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace ProjectManagement
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Project",
+                url: "Project/doanaction",
+                defaults: new { controller = "Project", action = "doanaction", name = UrlParameter.Optional });
         }
     }
 }
