95a8aeb75c614abaeda047544dfc398f0a53341f
unknown <ana.opra@XDR117.xoomworks.local>
2017-01-27 17:06:41 +0200

    Lab5 Ex2-3-4-5-6-7-8

--- ProjectManagement/Controllers/HomeController.cs
+++ ProjectManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,8 +17,14 @@ namespace ProjectManagement.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.Author = "Anna";
 
-            return View();
+            var model = new AboutModel();
+            model.Message = "Hello";
+            model.Author = "Opra";
+            return View(model);
+
+            // return View();
         }
 
         public ActionResult Contact()
