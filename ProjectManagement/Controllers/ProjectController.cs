bab1c51b629c9485d5b77796f079ccdbcdfeefc7
unknown <ana.opra@XDR117.xoomworks.local>
2017-01-27 17:15:07 +0200

    Lab6 Ex2

---
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult DoAnAction()
+        {
+            return Content("Hello world!");
+        }
+    }
+}
